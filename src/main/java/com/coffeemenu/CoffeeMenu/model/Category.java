543e6ec8d3f0ecc0985be2ff0c7417da5efd6e5d
jimmy <jimoseipeace2bb@gmail.com>
2025-04-01 20:32:08 -0500

    added @JsonCreator to convert to the enum types to uppercase

--- src/main/java/com/coffeemenu/CoffeeMenu/model/Category.java
+++ src/main/java/com/coffeemenu/CoffeeMenu/model/Category.java
@@ -1,5 +1,14 @@
 package com.coffeemenu.CoffeeMenu.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Category {
-    COFFEE,TEA,LATTE,SMOOTHIE,CAKE,ESPRESSO,MOCHA
+    COFFEE,TEA,LATTE,SMOOTHIE,CAKE,ESPRESSO,MACHO;
+
+
+    @JsonCreator
+    public static Category fromString(String name) {
+        return Category.valueOf(name.toUpperCase());
+    }
+
 }
